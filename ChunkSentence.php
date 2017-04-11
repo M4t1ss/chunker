e54253fd3d3118153e5a3084b088cfec284d4b50
Matīss Rikters <matiss_rikters@hotmail.com>
2017-04-11 17:23:08 +0300

    Die if missing a required parameter

--- ChunkSentence.php
+++ ChunkSentence.php
@@ -1,10 +1,10 @@
 <?php
 include 'chunk.php';
 
-$opts = getopt("m:s:g:");
-$minSize = $opts["m"];
-$sentence = $opts["s"];
-$grammar = $opts["g"];
+$opts 		= getopt("m:g:s:");
+$minSize 	= isset($opts["m"])?$opts["m"]:null;
+$grammar 	= isset($opts["g"])?$opts["g"]:die("Missing grammar parameter '-g'\n");
+$sentence 	= isset($opts["s"])?$opts["s"]:die("Missing input sentence parameter '-s'\n");
 
 //Parse the input sentence
 $parsed = shell_exec('echo "'.$sentence.'" | java -Xmx1024m -jar included/BerkeleyParser.jar -gr '.$grammar);
